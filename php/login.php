0f8c73a64b528add7e69e12336cff9beea627ca5
DaviAravechia <davi.aravechia@aluno.fmpsc.edu.br>
2026-02-20 12:23:15 -0300

    criando as tela principal da declaração, tela de resgate do presente e a de agradecimento

--- php/login.php
+++ php/login.php
@@ -2,7 +2,7 @@
                 $name = isset($_GET["name"])?$_GET["name"]:null;
                 $password = isset($_GET["password"])?$_GET["password"]:null;
                 if ($name == "sabrina viana mariano" && $password == "26/10/2025") {
-                    header('Location:../html/saas.html');
+                    header('Location:../html/birthday.html');
                     }
                     else {
                         $acess = "refused";
